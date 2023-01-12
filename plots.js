2aebf7fc39be120a1ea405676889d0cb761b8b72
godak <gabyodak@Gabys-Air.lan>
2023-01-12 12:56:10 -0500

    Updating

--- plots.js
+++ plots.js
@@ -11,7 +11,27 @@ function init() {
           .text(sample)
           .property("value", sample);
       });
-    });
+    }); 
+
+    // Grab a reference to the dropdown select element
+    //var selector = d3.select("#selDataset"); //Begin Deliverable 1 Script
+
+    // Use the list of sample names to populate the select options
+    //d3.json("samples.json").then((data) => {
+      //var sampleNames = data.names;
+
+    //sampleNames.forEach((sample) => {
+      //selector
+      //.append("option")
+      //.text(sample)
+      //.property("value", sample);
+      //});
+
+    // Use the first sample from the list to build the initial plots
+    //var firstSample = sampleNames[0];
+    //buildCharts(firstSample);
+    //buildMetadata(firstSample);
+  //});
 };
   
 init();
